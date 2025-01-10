f9925a20232953163f6d64a524acb942a3cbc533
SOHEL <117889326+Shaiksohel305@users.noreply.github.com>
2025-01-10 18:42:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,65 @@
+function bahubalidata() {
+    var imgdt = document.querySelector('img')
+    imgdt.src = "https://3.bp.blogspot.com/-pW6eYWKs8EY/VZ-Cou5nzDI/AAAAAAAAAkM/_B3JrGwgd_Y/s1600/Bahubali-1.jpg";
+    const main = document.getElementById('main');
+    main.innerText = "Bahubali:The Begining"
+    const title = document.getElementById('id');
+    title.innerText = "Bhahubali:The Begining"
+    const act = document.getElementById('act');
+    act.innerText = "Prabhas Rana Daggubati Anushka Shetty Tamannaah Bhatia Ramya Krishnan."
+
+    const date = document.getElementById('date');
+    date.innerText = "June 9 2015"
+    const dic = document.getElementById('dic');
+    dic.innerText = "SS Rajamouli"
+
+    const rate = document.getElementById('rate');
+    rate.innerText = "9.0"
+    const box = document.getElementById('box');
+    box.innerText = "980.Cr"
+    const para = document.getElementById('para');
+    para.innerText = "Baahubali: The Beginning is a 2015 Indian epic action film co-written and directed by S. S. Rajamouli, and produced by Shobu Yarlagadda and Prasad Devineni under Arka Media Works. The film was filmed in both Telugu and Tamil languages.[8][9] It features Prabhas in a dual role alongside Rana Daggubati, Anushka Shetty, Tamannaah Bhatia, Ramya Krishnan, Sathyaraj, and Nassar. The first of a duology of films, it follows Sivudu, an adventurous young man who helps his love Avantika rescue Devasena, "
+};
+function rrrdata() {
+    var imgdt = document.querySelector('img')
+    imgdt.src = "https://is3-ssl.mzstatic.com/image/thumb/Music122/v4/07/75/cd/0775cd95-da79-2e33-7802-c6898b5bc74b/8903431821683_cover.jpg/1200x1200bf-60.jpg";
+    const main = document.getElementById('main');
+    main.innerText = "RRR"
+    const title = document.getElementById('id');
+    title.innerText = "RRR"
+    const dic = document.getElementById('dic');
+    dic.innerText = "SS Rajamouli"
+    const date = document.getElementById('date');
+    date.innerText = "25 March 2022"
+    const act = document.getElementById('act');
+    act.innerText = "	N. T. Rama Rao Jr.Ram CharanAlia BhattAjay DevgnRay StevensonAlison Doody ."
+
+    const rate = document.getElementById('rate');
+    rate.innerText = "9.6"
+    const box = document.getElementById('box');
+    box.innerText = "1300.Cr"
+    const para = document.getElementById('para');
+    para.innerText = "RRR[b] (subtitled onscreen as Roudram Ranam Rudhiram) is a 2022 Indian Telugu-language epic historical action drama film directed by S. S. Rajamouli, who co-wrote the film with V. Vijayendra Prasad. It was produced by D. V. V. Danayya under DVV Entertainment. The film stars N. T. Rama Rao Jr., Ram Charan, Ajay Devgn, Alia Bhatt, Shriya Saran, Samuthirakani, Ray Stevenson, Alison Doody, and Olivia Morris. It is a fictionalized tribute. hriya Saran, Samuthirakani, Ray Stevenson, Alison Doody, and Olivia Morris. It is"
+};
+function pushpadata() {
+    var imgdt = document.querySelector('img')
+    imgdt.src = "https://m.media-amazon.com/images/M/MV5BNGZlNTFlOWMtMzUwNC00ZDdhLTk0MWUtOGZjYzFlOTBmNDdhXkEyXkFqcGdeQXVyMTUyNjIwMDEw._V1_.jpg";
+    const main = document.getElementById('main');
+    main.innerText = "Pushpa-2 The Rule"
+
+    const title = document.getElementById('id');
+    title.innerText = "Pushpa-2 The Rule"
+
+    const date = document.getElementById('date');
+    date.innerText = "5 December 2024"
+    const dic = document.getElementById('dic');
+    dic.innerText = "Sukumar"
+    const act = document.getElementById('act');
+    act.innerText = "AlluArjun,Rashmika,Sreeleela,Fahad fasil,Srikanth."
+    const rate = document.getElementById('rate');
+    rate.innerText = "8.3"
+    const box = document.getElementById('box');
+    box.innerText = "18.30.Cr"
+    const para = document.getElementById('para');
+    para.innerText = "Pushpa 2: The Rule is a 2024 Indian Telugu-language action drama film[5] directed by Sukumar and produced by Mythri Movie Makers, in association with Sukumar Writings. The film stars Allu Arjun in the titular role, alongside Rashmika Mandanna, Fahadh Faasil, Jagapathi Babu, Jagadeesh Prathap Bandari, Dhananjaya, Rao Ramesh, Sunil, Anasuya Bharadwaj and Ajay. It is the second instalment of the Pushpa film series and the sequel to Pushpa The film stars Allu Arjun in the titular role, alongside Rashmika Mandanna, Fahadh Faasil"
+}
